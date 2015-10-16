12ea29ef843bdbc340d3567107f67626febd4cd6
R vd Hoek <ralvdhoek@gmail.com>
2015-10-16 19:55:27 +0200

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,19 +1,18 @@
 <html>
 <HEAD>
-<meta name="robots" content="noindex, follow">
-
+<meta name="robots" content="noindex">
 <TITLE>Ping test</TITLE>
 </HEAD>
 
 <BODY BGCOLOR="WHITE">
-<H1>A simple ping test</H1>
+<table border="3" bordercolor="#c86260" bgcolor="#ffffcc" width="50%" cellspacing="5" cellpadding="3">
+        <tr><td><H1>A Simple Ping test</H1></td></tr>
+</table>
+<TABLE><TR><TD COLSPAN=2> </TD></TR></TABLE>
 
 <table border="3" bordercolor="#c86260" bgcolor="#ffffcc" width="50%" cellspacing="5" cellpadding="3">
-
 <tr><td align="center" colspan="2"><?php $strHeading = "<h1>Ping tested: " . $_POST["ping_servername"] . "</h1>"; echo $strHeading; ?></td></tr>
 <tr><td valign="top">
-
-
 
 <?php
 if (strpos($_POST["ping_servername"],'qwerty') !== false) {
@@ -41,13 +40,14 @@ if ("" == trim($_POST["ping_servername"])) {
     break;
 }
 
-                echo "ping -c ". $_POST["countvalue"]." ". $_POST["ping_servername"];
-                $output = shell_exec('ping -c'. $_POST["countvalue"].' '.$_POST["ping_servername"]);
+                echo "ping -c ". $_POST["countvalue"]." -t ". $_POST["ttlvalue"]." -s ". $_POST["packetvalue"]." ". $_POST["ping_servername"];
+                $output = shell_exec('ping -c'. $_POST["countvalue"].' -t '.$_POST["ttlvalue"].' -s '.$_POST["packetvalue"].' '.$_POST["ping_servername"]);
                 echo "<pre>$output</pre>";
 
 ?>
 
-        </tr>
+	</tr>
         </body>
 </head>
 </html>
+
