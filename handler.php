25ac1bd870d781395106530a18985e4cad6aec5f
R vd Hoek <ralvdhoek@gmail.com>
2016-03-06 20:48:43 +0100

    Update handler.php

--- handler.php
+++ handler.php
@@ -15,6 +15,28 @@
 <tr><td valign="top">
 
 <?php
+function disable_ob() {
+// Turn off output buffering
+ini_set('output_buffering', 'off');
+// Turn off PHP output compression
+ini_set('zlib.output_compression', false);
+//Flush (send) the output buffer and turn off output buffering
+//ob_end_flush();
+while (@ob_end_flush());
+// Implicitly flush the buffer(s)
+ini_set('implicit_flush', true);
+ob_implicit_flush(true);
+//prevent apache from buffering it for deflate/gzip
+header("Content-type: text/plain");
+header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+for($i = 0; $i < 1000; $i++)
+{
+echo '  ';
+}
+ob_flush();
+flush();
+}
+
 if (strpos($_POST["ping_servername"],'qwerty') !== false) {
     echo 'No valid ip/dns name.  '. $_POST["ping_servername"] ;
     break;
@@ -41,13 +63,17 @@ if ("" == trim($_POST["ping_servername"])) {
 }
 
                 echo "ping -c ". $_POST["countvalue"]." -t ". $_POST["ttlvalue"]." -s ". $_POST["packetvalue"]." ". $_POST["ping_servername"];
-                $output = shell_exec('ping -c'. $_POST["countvalue"].' -t '.$_POST["ttlvalue"].' -s '.$_POST["packetvalue"].' '.$_POST["ping_servername"]);
+        	disable_ob();
+                $output = shell_exec('ping -c'.escapeshellarg($_POST["countvalue"]).' -t '.escapeshellarg($_POST["ttlvalue"]).' -s '.escapeshellarg($_POST["packetvalue"]).' '.escapeshellarg($_POST["ping_servername"]).' 2>&1; echo $?');
                 echo "<pre>$output</pre>";
-
+	ob_flush();
+	flush();
 ?>
 
 	</tr>
         </body>
 </head>
 </html>
+
+
 
